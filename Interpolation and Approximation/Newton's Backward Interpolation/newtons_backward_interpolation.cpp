0a0f3fa2fbf47b31b0ceda724fe697943532a717
Utsa Roy <utsaroy3@gmail.com>
2025-12-14 00:47:24 +0600

    Newtons Backward Interpolation

---
+++ Interpolation and Approximation/Newton's Backward Interpolation/newtons_backward_interpolation.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+double fact(int n){
+    double f = 1.0;
+    for (int i=2; i<=n; i++) f*=i;
+    return f;
+}
+
+int main(){
+    cout<<"Enter num of data points: ";
+    int n; cin>>n;
+
+    cout<<"Enter x values: ";
+    vector<double> xs(n);
+    for (int i=0; i<n; i++) cin>>xs[i];
+
+    cout<<"Enter y values: ";
+    vector<double> ys(n);
+    for (int i=0; i<n; i++) cin>>ys[i];
+    
+    cout<<"Enter value to be interpolated: ";
+    double x; cin>>x;
+    
+    //Backward difference table
+    vector<vector<double>> diff(n, vector<double>(n, 0.0));
+    for (int i=0; i<n; i++) diff[i][0]=ys[i];
+    for (int j=1; j<n; j++){
+        for (int i=n-1; i>0; i--){
+            diff[i][j] = diff[i][j-1] - diff[i-1][j-1];
+        }
+    }
+    
+    double h=xs[1]-xs[0];
+    double v=(x-xs[n-1])/h;
+
+    //backward interpolation
+    double ans=diff[n-1][0];
+    double v_prod=1;
+    for (int k=1; k<n; k++){
+        v_prod *= (v+(k-1));
+        ans += (v_prod/fact(k))*diff[n-1][k];
+    }
+
+    cout<<fixed<<setprecision(2);
+    cout<<"\nBackward Difference Table:\n";
+    for (int i=0; i<n; i++){
+        for (int j=0; j<n; j++){
+            cout<<setw(5)<<diff[i][j]<<" ";
+        }
+        cout<<'\n';
+    }
+
+    cout<<"\nInterpolated value at x = "<<x<<" is f(x) = "<<ans<<"\n";
+    return 0;
+}
