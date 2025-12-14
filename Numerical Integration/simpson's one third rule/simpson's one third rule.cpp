e5ddb9e3f0a3fc153d27b998bd205b851466880c
Utsa Roy <utsaroy3@gmail.com>
2025-12-15 00:29:06 +0600

    simpsons 1/3 and 3/8 rule

---
+++ Numerical Integration/simpson's one third rule/simpson's one third rule.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+double fun(double x, vector<double> crr){
+    double res=0; int n=crr.size();
+    for(int i=0; i<n; i++){
+        res += crr[i]*pow(x, n-1-i);
+    }
+    return res;
+}
+
+double simpson(double a, double b, double h, vector<double> crr){
+    double sum=fun(a, crr)+fun(b, crr);
+    for(double i=1; a+i*h<b; i++){
+        if((int)i%2==0) sum += 2*fun(a+i*h, crr);
+        else sum += 4*fun(a+i*h, crr);
+    }
+    return (h*sum)/3.0;
+}
+
+int main(){
+    cout<<"Degree of the function:\n";
+    int d; cin>>d;
+    cout<<"Coefficients of the function:\n";
+    vector<double> crr(d+1); for(auto &i: crr) cin>>i;
+
+    cout<<"Upper limit:\n";
+    double ul; cin>>ul;
+
+    cout<<"Lower limit:\n";
+    double ll; cin>>ll;
+
+    cout<<"Number of intervals:\n";
+    double n; cin>>n;
+
+    double h = (ul-ll)/n;
+    double ans = simpson(ll, ul, h, crr);
+
+    //output
+    cout<<"The polynomial:\nf(x) = "<<crr[0]<<"x^"<<d;
+    for(int i=0; i<=d; i++) cout<<" + "<<crr[i]<<"x^"<<d-i;
+    cout<<endl;
+
+    cout<<"Definite integral of the polynomial = "<<ans<<endl;
+    return 0;
+}
